fa8dd232e7fd8062113aa894c3ca90e72cfabcbd
Sarah <sarah_hudon14@hotmail.com>
2025-03-06 13:16:37 -0400

    'ajur'

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("Script chargé avec succès !");
+
     const services = document.querySelectorAll(".service-box");
 
     services.forEach(service => {
         service.addEventListener("mouseenter", () => {
-            service.style.boxShadow = "0px 0px 20px rgba(255, 255, 255, 0.7)";
+            service.style.boxShadow = "0px 0px 20px rgba(255, 255, 255, 0.8)";
         });
 
         service.addEventListener("mouseleave", () => {
-            service.style.boxShadow = "none";
+            service.style.boxShadow = "0px 5px 15px rgba(0, 0, 0, 0.5)";
         });
 
         service.addEventListener("click", () => {
